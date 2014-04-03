7024512f4c634697a38e5bda4677c98bb3e30c4a
anaramos <anaramos_50@yahoo.com.ar>
2014-04-03 02:41:22 -0300

    Segundo Commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo Git");
+		for (int i=0;i<2;i++)
+			System.out.println("Hola mundo Git");
 	}
 
 }
