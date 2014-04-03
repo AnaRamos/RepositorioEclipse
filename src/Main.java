126a95ffa5af51fa367f31172df220d73cbd4802
anaramos <anaramos_50@yahoo.com.ar>
2014-04-03 06:56:08 -0300

    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		for (int i=0;i<2;i++)
 			System.out.println("Hola mundo Git");
 		BigFactorial fact = new BigFactorial();
-		System.out.println(fact.factorial(100));
+		System.out.println(fact.factorial(200));
 	}
 
 }
